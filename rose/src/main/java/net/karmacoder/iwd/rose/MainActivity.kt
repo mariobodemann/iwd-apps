4a35af2d1ca28c42d555e83234000fc42eca3d37
Mario Bodemann <mario.bodemann@mobimeo.com>
2021-03-06 14:51:45 +0100

    Adding Suzanne
    Remember to tell blender to use the system python, not the build in one, otherwise the glf export will not work.

--- rose/src/main/java/net/karmacoder/iwd/rose/MainActivity.kt
+++ rose/src/main/java/net/karmacoder/iwd/rose/MainActivity.kt
@@ -1,11 +1,90 @@
 package net.karmacoder.iwd.rose
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Choreographer
+import android.view.SurfaceView
+import com.google.android.filament.Filament
+import com.google.android.filament.Skybox
+import com.google.android.filament.gltfio.Gltfio
+import com.google.android.filament.utils.KtxLoader
+import com.google.android.filament.utils.ModelViewer
+import com.google.android.filament.utils.Utils
+import java.nio.ByteBuffer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    companion object {
+        init {
+            Utils.init()
+            Filament.init()
+        }
+    }
+
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var choreographer: Choreographer
+    private lateinit var modelViewer: ModelViewer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        surfaceView = findViewById(R.id.main_surface_view)
+
+        choreographer = Choreographer.getInstance()
+        modelViewer = ModelViewer(surfaceView)
+
+        surfaceView.setOnTouchListener(modelViewer)
+
+        loadGlb("monkey")
+        loadEnvironment("venetian_crossroads_2k")
+    }
+
+    private fun loadEnvironment(ibl: String) {
+        // Create the indirect light source and add it to the scene.
+        var buffer = readAsset("envs/$ibl/${ibl}_ibl.ktx")
+        KtxLoader.createIndirectLight(modelViewer.engine, buffer).apply {
+            intensity = 50_000f
+            modelViewer.scene.indirectLight = this
+        }
+
+        // Create the sky box and add it to the scene.
+        buffer = readAsset("envs/$ibl/${ibl}_skybox.ktx")
+        KtxLoader.createSkybox(modelViewer.engine, buffer).apply {
+            modelViewer.scene.skybox = this
+        }
+    }
+
+    private fun loadGlb(name: String) {
+        val buffer = readAsset("models/${name}.glb")
+        modelViewer.loadModelGlb(buffer)
+        modelViewer.transformToUnitCube()
+    }
+
+    private fun readAsset(assetName: String): ByteBuffer {
+        val input = assets.open(assetName)
+        val bytes = ByteArray(input.available())
+        input.read(bytes)
+        return ByteBuffer.wrap(bytes)
+    }
+
+    private val frameCallback = object : Choreographer.FrameCallback {
+        override fun doFrame(currentTime: Long) {
+            choreographer.postFrameCallback(this)
+            modelViewer.render(currentTime)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        choreographer.postFrameCallback(frameCallback)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        choreographer.removeFrameCallback(frameCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        choreographer.removeFrameCallback(frameCallback)
     }
 }
